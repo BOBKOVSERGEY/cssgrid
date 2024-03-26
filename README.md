9a12a6dee43ff61dedcc0cafe61a336945e46932
bobkovs <aastudiya@gmail.com>
2024-03-26 13:15:03 +0300

    grid-template-row grid template-column

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# cssgrid
-cssgrid
+
+#emits command
+.container>.item.item--${some}*6
---
+++ app.js
@@ -0,0 +1,8 @@
+let bs = require("browser-sync").create();
+
+bs.watch(['*.*', '*/*/*.*', '*/*.*']).on("change", bs.reload);
+
+// Start a Browsersync proxy
+bs.init({
+    server: "./"
+});
